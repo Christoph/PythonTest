6761c6f203e6b0d4543d987047454260558e5773
Chris <christoph.kralj@gmail.com>
2015-06-10 13:31:21 +0200

    Added reloadall modules method

--- tagrefinery/main.py
+++ tagrefinery/main.py
@@ -1,10 +1,18 @@
+# Lib imports
 import pandas as pd
 import numpy as np
 import nltk as nlp
 import matplotlib.pyplot as plt
+import importlib
 
+# Custom modulemodules
 import database
 
+# Reload custom modules
+def reloadall():
+    global database
+    database = importlib.reload(database)
+    
 print("Begin Main")
 
 db = database.InitializeDB()
