9f44fd2b2e63eb432d8c69f1f6de7590aa5c561d
Chris <christoph.kralj@gmail.com>
2015-06-10 11:38:44 +0200

    Updated db schema and scripts.

--- db/clear_db.sh
+++ db/clear_db.sh
@@ -1,2 +1,2 @@
-mysql -u root tags -e "Drop table TT; Drop table Track; Drop table Tag; Drop table Artist"
+mysql -u root tags -e "Drop table TT; Drop table AT; Drop table Ancestor; Drop table Track; Drop table Tag; Drop table Artist"
 mysql -u root tags < db_schema
